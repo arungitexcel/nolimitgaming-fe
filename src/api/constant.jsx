0383c9a27e463152b19500b770119385821417dd
arungitexcel <arungit.excel2011@gmail.com>
2026-02-24 15:31:44 +0530

    fixed categories issue

--- src/api/constant.jsx
+++ src/api/constant.jsx
@@ -3,6 +3,7 @@ let BASE_API_URL, CLIENT_URL;
 if (import.meta.env.VITE_DEV == "true") {
   // npm run dev command
   BASE_API_URL = import.meta.env.VITE_API_URL_DEV;
+  console.log("BASE_API_URL", BASE_API_URL);
   CLIENT_URL = import.meta.env.VITE_DEV_CLIENT_URL;
 } else if (import.meta.env.MODE == "production") {
   // for npm run build command
